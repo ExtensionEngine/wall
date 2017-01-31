91f55ff82eac810ea526cbc4c35d1b777fef670b
droguljic <droguljic@extensionengine.com>
2017-01-31 14:49:04 +0100

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ['semistandard'],
+  rules: {
+    'no-console': 'warn',
+    'global-require': 'error',
+    'space-before-function-paren': ['error', 'never']
+  }
+};

69c702ed9c1a5c17660f1dc140544f72f1edb289
droguljic <droguljic@extensionengine.com>
2017-05-18 19:30:28 +0200

    Updated linter rules to define generator star spacing

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
   rules: {
     'no-console': 'warn',
     'global-require': 'error',
+    'generator-star-spacing': ['error', { before: false, after: false }],
     'space-before-function-paren': ['error', 'never']
   }
 };
